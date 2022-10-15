c234cd4f6f93c7d2cbd9b61bd09a293363cf6f69
bzr-lipe <bzr.lipe@gmail.com>
2022-10-14 23:11:06 -0300

    sprint 04 finalizado

--- sprint04/src/App.java
+++ sprint04/src/App.java
@@ -20,15 +20,19 @@ public class App {
 		System.out.println("Quantas Equipes Participarão?");
 		nEquipes = sc.nextInt();
 
+		int opcao;
+
 		int idEquipe[] = new int[nEquipes];
 		int pontos[][] = new int[nEquipes][nEquipes - 1];
 		int notaDesign[] = new int[nEquipes];
 		int totalPontos[] = new int[nEquipes];
 		int totalPontosFase1[] = new int[nEquipes];
+		String nomeRobo[] = new String[nEquipes];
 		int vitorias[] = new int[nEquipes];
 		int derrotas[] = new int[nEquipes];
 		int empates[] = new int[nEquipes];
 		int vEmpates[] = new int[nEquipes];
+		int opcaoNomes;
 
 		for (int i = 0; i < idEquipe.length; i++) {
 			idEquipe[i] = (i + 1);
@@ -44,19 +48,34 @@ public class App {
 			notaDesign[i] = generator.nextInt(10);
 		}
 
-		
-	
+		System.out.println("\nNome das Equipes: ");
+		System.out.println("1) Desejo Inserir Nomes Manualmente\n2) Desejo que a máquine nomeie as Equipes;");
+		opcaoNomes = sc.nextInt();
+		sc.nextLine();
+
+		if (opcaoNomes == 1) {
+			for (int i = 0; i < nomeRobo.length; i++) {
+				System.out.println("\nDigite o nome da equipe " + (i + 1) + ": ");
+				nomeRobo[i] = sc.nextLine();
+			}
+		} else if (opcaoNomes == 2) {
+			System.out.println("\nIDS serão gerados de 1 a " + nEquipes);
+		} else {
+			System.out.println("\nOpção Inválida");
+			System.out.println("IDS serão gerados de 1 a " + nEquipes);
+		}
+
 		int nBatalha = 0;
 
 		// coletar total
 
-		System.out.println("Como Deseja assistir a primeira rodada ?");
+		System.out.println("\nComo Deseja assistir a primeira rodada ?");
 		System.out.println("1 --> Mostrar todas as Lutas");
 		System.out.println("2 --> Mostrar resumo das lutas");
-		int opcao = sc.nextInt();
+		opcao = sc.nextInt();
 
-		System.out.println("    Resultados: \n"); 
-		
+		System.out.println("    Resultados: \n");
+
 		switch (opcao) {
 		case 1:
 			System.out.println();
@@ -70,25 +89,42 @@ public class App {
 						System.out.println("     Batalha " + nBatalha);
 						System.out.println("---------------------");
 
-						System.out.println(" Equipe " + idEquipe[i] + " x " + "Equipe " + idEquipe[j]);
+						if (opcaoNomes == 1) {
+							System.out.println("  " + nomeRobo[i] + " x " + nomeRobo[j]);
+						} else {
+							System.out.println(" Equipe " + idEquipe[i] + " x " + "Equipe " + idEquipe[j]);
+
+						}
 						System.out.println("        " + pontos[i][j - 1] + " x " + pontos[j][i]);
 
 						if (pontos[i][j - 1] == pontos[j][i]) {
 							System.out.println("****** EMPATE ********");
 							System.out.println("    Notas Design:");
-							System.out.println("    Equipe " + idEquipe[i] + " = " + notaDesign[i]);
-							System.out.println("    Equipe " + idEquipe[j] + " = " + notaDesign[j]);
-
+							if (opcaoNomes == 1) {
+								System.out.println("    " + nomeRobo[i] + " = " + notaDesign[i]);
+								System.out.println("    " + nomeRobo[j] + " = " + notaDesign[j]);
+							} else {
+								System.out.println("    Equipe " + idEquipe[i] + " = " + notaDesign[i]);
+								System.out.println("    Equipe " + idEquipe[j] + " = " + notaDesign[j]);
+							}
 							System.out.println("---------------------");
 							if (notaDesign[i] > notaDesign[j]) {
-								System.out.println("  Equipe " + idEquipe[i] + " venceu!");
+								if (opcaoNomes == 1) {
+									System.out.println("    " + nomeRobo[i] + " venceu!");
+								} else {
+									System.out.println("  Equipe " + idEquipe[i] + " venceu!");
+								}
 								totalPontos[i] += 3;
 								totalPontosFase1[i] += 3;
 								empates[i]++;
 								vEmpates[i]++;
 								derrotas[j]++;
 							} else {
-								System.out.println("  Equipe " + idEquipe[j] + " venceu!");
+								if (opcaoNomes == 1) {
+									System.out.println("    " + nomeRobo[j] + " venceu!");
+								} else {
+									System.out.println("  Equipe " + idEquipe[j] + " venceu!");
+								}
 								totalPontos[j] += 3;
 								totalPontosFase1[j] += 3;
 								derrotas[i]++;
@@ -98,13 +134,21 @@ public class App {
 						} else {
 							System.out.println("---------------------");
 							if (pontos[i][j - 1] > pontos[j][i]) {
-								System.out.println("  Equipe " + idEquipe[i] + " venceu!");
+								if (opcaoNomes == 1) {
+									System.out.println("    " + nomeRobo[i] + " venceu!");
+								} else {
+									System.out.println("  Equipe " + idEquipe[i] + " venceu!");
+								}
 								totalPontos[i] += 5;
 								totalPontosFase1[i] += 5;
 								vitorias[i]++;
 								derrotas[j]++;
 							} else {
-								System.out.println("  Equipe " + idEquipe[j] + " venceu!");
+								if (opcaoNomes == 1) {
+									System.out.println("    " + nomeRobo[j] + " venceu!");
+								} else {
+									System.out.println("  Equipe " + idEquipe[j] + " venceu!");
+								}
 								totalPontos[j] += 5;
 								totalPontosFase1[j] += 5;
 								vitorias[j]++;
@@ -156,7 +200,14 @@ public class App {
 			}
 
 			for (int i = 0; i < vitorias.length; i++) {
-				System.out.print("Equipe " + (i + 1) + ": " + vitorias[i] + " vitórias, " + derrotas[i] + " derrotas");
+				if (opcaoNomes == 1) {
+					System.out.print(
+							"Equipe " + nomeRobo[i] + ": " + vitorias[i] + " vitórias, " + derrotas[i] + " derrotas");
+				} else {
+					System.out.print(
+							"Equipe " + (i + 1) + ": " + vitorias[i] + " vitórias, " + derrotas[i] + " derrotas");
+
+				}
 				if (empates[i] >= 1) {
 					if (empates[i] > 1) {
 						System.out.println(", " + empates[i] + " empates (" + vEmpates[i] + " vitórias).");
@@ -177,6 +228,7 @@ public class App {
 
 		// Organizar TOP3
 		int aux1, aux2, aux3;
+		String aux4;
 
 		for (int i = 0; i < nEquipes - 1; i++) {
 			for (int j = i + 1; j < nEquipes; j++) {
@@ -192,6 +244,12 @@ public class App {
 					aux3 = notaDesign[j];
 					notaDesign[j] = notaDesign[i];
 					notaDesign[i] = aux3;
+
+					if (opcaoNomes == 1) {
+						aux4 = nomeRobo[j];
+						nomeRobo[j] = nomeRobo[i];
+						nomeRobo[i] = aux4;
+					}
 				}
 
 				if (totalPontos[j] == totalPontos[i]) {
@@ -207,6 +265,12 @@ public class App {
 						aux3 = notaDesign[j];
 						notaDesign[j] = notaDesign[i];
 						notaDesign[i] = aux3;
+
+						if (opcaoNomes == 1) {
+							aux4 = nomeRobo[j];
+							nomeRobo[j] = nomeRobo[i];
+							nomeRobo[i] = aux4;
+						}
 					}
 				}
 			}
@@ -215,21 +279,44 @@ public class App {
 		System.out.println("TOP 3:\n");
 
 		if (totalPontos[0] == totalPontos[1]) {
-			System.out.println("1) Equipe " + idEquipe[0] + ": " + totalPontos[0] + " Pontos --> Nota de Design: "
-					+ notaDesign[0]);
-			System.out.println("2) Equipe " + idEquipe[1] + ": " + totalPontos[1] + " Pontos --> Nota de Design: "
-					+ notaDesign[1]);
-			System.out.println("3) Equipe " + idEquipe[2] + ": " + totalPontos[2] + " Pontos");
-		} else if (totalPontos[1] == totalPontos[2]) {
-			System.out.println("1) Equipe " + idEquipe[0] + ": " + totalPontos[0] + " Pontos");
-			System.out.println("2) Equipe " + idEquipe[1] + ": " + totalPontos[1] + " Pontos --> Nota de Design: "
-					+ notaDesign[1]);
-			System.out.println("3) Equipe " + idEquipe[2] + ": " + totalPontos[2] + " Pontos --> Nota de Design: "
-					+ notaDesign[2]);
-		} else {
-			System.out.println("1) Equipe " + idEquipe[0] + ": " + totalPontos[0] + " Pontos");
-			System.out.println("2) Equipe " + idEquipe[1] + ": " + totalPontos[1] + " Pontos");
-			System.out.println("3) Equipe " + idEquipe[2] + ": " + totalPontos[2] + " Pontos");
+			if (opcaoNomes == 1) {
+				System.out.println("1) Equipe " + nomeRobo[0] + ": " + totalPontos[0] + " Pontos --> Nota de Design: "
+						+ notaDesign[0]);
+				System.out.println("2) Equipe " + nomeRobo[1] + ": " + totalPontos[1] + " Pontos --> Nota de Design: "
+						+ notaDesign[1]);
+				System.out.println("3) Equipe " + nomeRobo[2] + ": " + totalPontos[2] + " Pontos");
+			} else {
+				System.out.println("1) Equipe " + idEquipe[0] + ": " + totalPontos[0] + " Pontos --> Nota de Design: "
+						+ notaDesign[0]);
+				System.out.println("2) Equipe " + idEquipe[1] + ": " + totalPontos[1] + " Pontos --> Nota de Design: "
+						+ notaDesign[1]);
+				System.out.println("3) Equipe " + idEquipe[2] + ": " + totalPontos[2] + " Pontos");
+			}
+		} if (totalPontos[1] == totalPontos[2]) {
+			if (opcaoNomes == 1) {
+				System.out.println("1) Equipe " + nomeRobo[0] + ": " + totalPontos[0] + " Pontos");
+				System.out.println("2) Equipe " + nomeRobo[1] + ": " + totalPontos[1] + " Pontos --> Nota de Design: "
+						+ notaDesign[1]);
+				System.out.println("3) Equipe " + nomeRobo[2] + ": " + totalPontos[2] + " Pontos --> Nota de Design: "
+						+ notaDesign[2]);
+			} else {
+				System.out.println("1) Equipe " + idEquipe[0] + ": " + totalPontos[0] + " Pontos");
+				System.out.println("2) Equipe " + idEquipe[1] + ": " + totalPontos[1] + " Pontos --> Nota de Design: "
+						+ notaDesign[1]);
+				System.out.println("3) Equipe " + idEquipe[2] + ": " + totalPontos[2] + " Pontos --> Nota de Design: "
+						+ notaDesign[2]);
+			}
+
+		} if (totalPontos[1] != totalPontos[2] && totalPontos[0] != totalPontos[1]) {
+			if (opcaoNomes == 1) {
+				System.out.println("1) Equipe " + nomeRobo[0] + ": " + totalPontos[0] + " Pontos");
+				System.out.println("2) Equipe " + nomeRobo[1] + ": " + totalPontos[1] + " Pontos");
+				System.out.println("3) Equipe " + nomeRobo[2] + ": " + totalPontos[2] + " Pontos");
+			} else {
+				System.out.println("1) Equipe " + idEquipe[0] + ": " + totalPontos[0] + " Pontos");
+				System.out.println("2) Equipe " + idEquipe[1] + ": " + totalPontos[1] + " Pontos");
+				System.out.println("3) Equipe " + idEquipe[2] + ": " + totalPontos[2] + " Pontos");
+			}
 		}
 
 		System.out.println("\n---------------------------");
@@ -240,8 +327,8 @@ public class App {
 		System.out.println("1 --> Mostrar todas as Lutas");
 		System.out.println("2 --> Mostrar resumo das lutas");
 		opcao = sc.nextInt();
-		
-		System.out.println("    Resultados:"); 
+
+		System.out.println("    Resultados:");
 
 		int pontosFINAL[][] = new int[3][2];
 
@@ -251,6 +338,9 @@ public class App {
 			}
 		}
 
+		for (int i = 0; i < pontosFINAL.length; i++) {
+			totalPontosFase1[i]=totalPontos[i];
+		}
 		// zerando arrays
 		for (int i = 0; i < nEquipes; i++) {
 			vitorias[i] = 0;
@@ -274,25 +364,46 @@ public class App {
 						System.out.println("     Batalha " + nBatalha);
 						System.out.println("---------------------\n");
 
-						System.out.println(" Equipe " + idEquipe[i] + " x " + "Equipe " + idEquipe[j]);
+						if (opcaoNomes == 1) {
+							System.out.println("    " + nomeRobo[i] + " x " + nomeRobo[j]);
+						} else {
+							System.out.println(" Equipe " + idEquipe[i] + " x " + "Equipe " + idEquipe[j]);
+
+						}
 						System.out.println("        " + pontosFINAL[i][j - 1] + " x " + pontosFINAL[j][i]);
 
-						if (pontos[i][j - 1] == pontos[j][i]) {
+						if (pontosFINAL[i][j - 1] == pontosFINAL[j][i]) {
 							System.out.println("****** EMPATE ********");
 							System.out.println("    Notas Design:");
-							System.out.println("    Equipe " + idEquipe[i] + " = " + notaDesign[i]);
-							System.out.println("    Equipe " + idEquipe[j] + " = " + notaDesign[j]);
+							if (opcaoNomes == 1) {
+								System.out.println("    " + nomeRobo[i] + " = " + notaDesign[i]);
+								System.out.println("    " + nomeRobo[j] + " = " + notaDesign[j]);
+							} else {
+								System.out.println("    Equipe " + idEquipe[i] + " = " + notaDesign[i]);
+								System.out.println("    Equipe " + idEquipe[j] + " = " + notaDesign[j]);
+							}
+
 							System.out.println("---------------------");
 
 							if (notaDesign[i] > notaDesign[j]) {
-								System.out.println("  Equipe " + idEquipe[i] + " venceu!");
+								if (opcaoNomes == 1) {
+									System.out.println("    " + nomeRobo[i] + " venceu!");
+								} else {
+									System.out.println("  Equipe " + idEquipe[i] + " venceu!");
+
+								}
 								empates[i]++;
 								vEmpates[i]++;
 								derrotas[j]++;
 								totalPontos[i] += 1;
 
 							} else {
-								System.out.println("  Equipe " + idEquipe[j] + " venceu!");
+								if (opcaoNomes == 1) {
+									System.out.println("    " + nomeRobo[j] + " venceu!");
+								} else {
+									System.out.println("  Equipe " + idEquipe[j] + " venceu!");
+
+								}
 								empates[j]++;
 								vEmpates[j]++;
 								derrotas[i]++;
@@ -302,12 +413,22 @@ public class App {
 						} else {
 							System.out.println("---------------------");
 							if (pontosFINAL[i][j - 1] > pontosFINAL[j][i]) {
-								System.out.println("  Equipe " + idEquipe[i] + " venceu!");
+								if (opcaoNomes == 1) {
+									System.out.println("    " + nomeRobo[i] + " venceu!");
+								} else {
+									System.out.println("  Equipe " + idEquipe[i] + " venceu!");
+
+								}
 								totalPontos[i] += 2;
 								vitorias[i]++;
 								derrotas[j]++;
 							} else {
-								System.out.println("  Equipe " + idEquipe[j] + " venceu!");
+								if (opcaoNomes == 1) {
+									System.out.println("    " + nomeRobo[j] + " venceu!");
+								} else {
+									System.out.println("  Equipe " + idEquipe[j] + " venceu!");
+
+								}
 								totalPontos[j] += 2;
 								vitorias[j]++;
 								derrotas[i]++;
@@ -327,20 +448,17 @@ public class App {
 				for (int j = (i + 1); j < pontosFINAL[i].length + 1; j++) {
 					if (idEquipe[i] != idEquipe[j]) {
 						nBatalha++;
-						if (pontos[i][j - 1] == pontos[j][i]) {
+						if (pontosFINAL[i][j - 1] == pontosFINAL[j][i]) {
 							if (notaDesign[i] > notaDesign[j]) {
 								empates[i]++;
 								vEmpates[i]++;
 								derrotas[j]++;
 								totalPontos[i] += 1;
-								totalPontosFase1[i] += 1;
 							} else {
 								empates[j]++;
 								vEmpates[j]++;
 								derrotas[i]++;
 								totalPontos[j] += 1;
-								totalPontosFase1[j] += 1;
-
 							}
 						} else {
 							if (pontosFINAL[i][j - 1] > pontosFINAL[j][i]) {
@@ -360,7 +478,11 @@ public class App {
 
 			}
 			for (int i = 0; i < 3; i++) {
-				System.out.print("Equipe " + idEquipe[i] + ": " + vitorias[i] + " vitórias, " + derrotas[i] + " derrotas");
+				if (opcaoNomes == 1) {
+					System.out.print("Equipe " + nomeRobo[i] + ": " + vitorias[i] + " vitórias, " + derrotas[i] + " derrotas");
+				}else {
+					System.out.print("Equipe " + idEquipe[i] + ": " + vitorias[i] + " vitórias, " + derrotas[i] + " derrotas");
+				}
 				if (empates[i] >= 1) {
 					if (empates[i] > 1) {
 						System.out.println(", " + empates[i] + " empates (" + vEmpates[i] + " vitórias).");
@@ -377,9 +499,7 @@ public class App {
 			break;
 
 		}
-		
-		
-		
+
 		for (int i = 0; i < pontosFINAL.length; i++) {
 			for (int j = i + 1; j < pontosFINAL[i].length; j++) {
 				if (totalPontos[j] > totalPontos[i]) {
@@ -394,18 +514,28 @@ public class App {
 					aux3 = notaDesign[j];
 					notaDesign[j] = notaDesign[i];
 					notaDesign[i] = aux3;
-				
-					}
+					
+					aux4 = nomeRobo[j];
+					nomeRobo[j] = nomeRobo[i];
+					nomeRobo[i] = aux4;
+
 				}
 			}
-		
-		System.out.println();
-		for (int i = 0; i <1; i++) {
-			System.out.println("A equipe campeã é:");
-			System.out.println("Equipe " +idEquipe[i] +" com " +(totalPontos[i]+totalPontosFase1[i]) +" pontos (" +totalPontosFase1[i] +" pontos na primeira rodada, "  +totalPontos[i] +" na segunda rodada)");
-
 		}
-	
+
+		System.out.println();
+		for (int i = 0; i < 1; i++) {
+			System.out.println("A equipe campeã é:");
+			if (opcaoNomes == 1) {
+				System.out.println("Equipe " + nomeRobo[i] + " com " + (totalPontos[i] + totalPontosFase1[i]) + " pontos ("
+						+ totalPontosFase1[i] + " pontos na primeira rodada, " + totalPontos[i] + " na segunda rodada)");
+			}else {
+				System.out.println("Equipe " + idEquipe[i] + " com " + (totalPontos[i] + totalPontosFase1[i]) + " pontos ("
+						+ totalPontosFase1[i] + " pontos na primeira rodada, " + totalPontos[i] + " na segunda rodada)");
+
+			}
+			
+		}
 
 		// gambiarra made by felipe
 	}
